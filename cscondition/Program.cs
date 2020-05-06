6769389a8ed66fe0008d6fd29106d32b14d29f13
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-06 14:17:42 +0900

    if문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -10,6 +10,18 @@ namespace cscondition
     {
         static void Main(string[] args)
         {
+            Console.Write("숫자 입력 : ");
+            var input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("짝수입니다~");
+            }
+            if (input % 2 == 1)
+            {
+                Console.WriteLine("홀수입니다~");
+            }
+
         }
     }
 }
