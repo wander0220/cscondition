0c2099fd76c36630b160f3cd11f128d4df938e7f
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-06 14:58:23 +0900

    #2 switch 조건문 실습예제

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -54,6 +54,35 @@ namespace cscondition
                     break;
             }
 
+            Console.Write("이번 달은 몇 월 인가요? : ");
+            int input2 = int.Parse(Console.ReadLine());
+            switch (input2)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다.");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다.");
+                    break;
+                default:
+                    Console.WriteLine("어느 계절에 사시는 건가요....?");
+                    break;
+            }
+
         }
     }
 }
