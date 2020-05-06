6224c7de91ab8bd485f30040e3e5ceb8be221c70
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-06 14:24:44 +0900

    if else 문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -17,9 +17,27 @@ namespace cscondition
             {
                 Console.WriteLine("짝수입니다~");
             }
-            if (input % 2 == 1)
+            else
             {
                 Console.WriteLine("홀수입니다~");
+            }
+
+            //현재 시간 구하기
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month); 
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            //오전과 오후 구분
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전입니다");
+            }
+            else 
+            {
+                Console.WriteLine("오후입니다");
             }
 
         }
