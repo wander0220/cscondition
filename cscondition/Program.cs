843825137309454a8f6918e353c39fcf5f2b7da4
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-06 14:29:57 +0900

    중첩 if문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -33,11 +33,19 @@ namespace cscondition
             //오전과 오후 구분
             if (DateTime.Now.Hour < 12)
             {
-                Console.WriteLine("오전입니다");
+                Console.WriteLine("아침 먹을 시간입니다");
             }
             else 
             {
-                Console.WriteLine("오후입니다");
+                if (DateTime.Now.Hour < 15)
+                {
+                    Console.WriteLine("점심 먹을 시간입니다. ");
+                }
+                else
+                {
+                    Console.WriteLine("저녁 먹을 시간입니다");
+                }
+              
             }
 
         }
